7deeb285869b32928b3f2b11b64a84388cafbb78
Bryan <delacruz2130195@mls.ceu.edu.ph>
2024-11-05 11:57:56 +0800

    commit

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Payments
+/// Payments
 router.get('/', (request, response) => {
     response.status(200).json({ message: 'GET all payments' });
 });
